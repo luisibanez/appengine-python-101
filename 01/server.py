d82588adaded8943543b9f0300e0d683925496ce
Luis Ibanez <luis.ibanez@gmail.com>
2015-05-23 21:12:50 -0400

    Simplify by removing user login.

--- 01/server.py
+++ 01/server.py
@@ -1,6 +1,4 @@
 
-from google.appengine.api import users
-
 import webapp2
 
 
@@ -10,14 +8,6 @@ class MainPage(webapp2.RequestHandler):
 
       self.response.set_status(200)
       self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
-
-      if users.get_current_user():
-        url = users.create_logout_url(self.request.uri)
-        url_linktext = 'Logout'
-      else:
-        url = users.create_login_url(self.request.uri)
-        url_linktext = 'Login'
-
 
       self.response.out.write('Hello World')
 
